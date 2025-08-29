6e0dad9e1ed62e09dc1f808b6bcca78a983abdb0
asciibase64 <ilsao@outlook.com>
2025-08-29 09:51:18 -0700

    update solution for syscall

--- syscall/kernel/defs.h
+++ syscall/kernel/defs.h
@@ -109,7 +109,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 int             trace(int);
-int            sysinfo(struct sysinfo *);
+int            	sysinfo(struct sysinfo *);
 
 // swtch.Sstruct sysinfo;
 void            swtch(struct context*, struct context*);
